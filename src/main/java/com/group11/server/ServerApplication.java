2b698139b93b6198f2c827b90c86dbe974b7be36
dusenadam <anilutku600@gmail.com>
2022-04-22 19:43:32 +0300

    Directory and hierarchy updated.

--- src/main/java/com/group11/server/ServerApplication.java
+++ src/main/java/com/group11/server/ServerApplication.java
@@ -3,8 +3,10 @@ package com.group11.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
